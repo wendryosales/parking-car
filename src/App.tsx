bc62167903961a3e5772c5ada7d65b5b93e61ea7
Wendryo S <wendryo.sales@gmail.com>
2022-04-27 00:20:40 -0300

    Install styled-components and create initial container

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
-import './App.css';
+import * as C from './App.styles';
 
-function App() {
-	return (
-		<div className="App">
-		</div>
-	);
+const App = () => {
+  return(
+    <C.Container>
+      <form>
+        <input
+          type="text"
+          name="name"
+          placeholder="vehicle name"
+          autoComplete="off"
+        />
+        <input
+          type="text"
+          name="license"
+          placeholder="license plate"
+          autoComplete="off"
+        />
+        <button type="button">Register</button>
+      </form>
+    </C.Container>
+  );
 }
 
 export default App;
